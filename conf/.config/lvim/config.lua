003adcc3c05b83fd51b34c56231aa21806688f61
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-09-21 09:03:52 -0700

    feat(lunarvim): init custom config

---
+++ conf/.config/lvim/config.lua
@@ -0,0 +1,10 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.undofile = false      -- persistent undo
+vim.opt.whichwrap = "b,s"     -- allow left/right cursor keys to move prev/next
+
+vim.opt.spellfile = "~/.local/share/nvim/spell/en.utf-8.add"
