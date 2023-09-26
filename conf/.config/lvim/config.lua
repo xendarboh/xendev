518fbcb75b2cc41129bbfcc9fffe47eaf7ec87cb
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-09-25 23:15:28 -0700

    feat(lunarvim): format on save, enable formatter prettier

--- conf/.config/lvim/config.lua
+++ conf/.config/lvim/config.lua
@@ -24,6 +24,26 @@ lvim.builtin.which_key.mappings["l"]["f"] = {
   "Format",
 }
 
+lvim.format_on_save.enabled = true
+lvim.format_on_save.pattern = {
+  "*.go",
+  "*.lua",
+}
+
+-- https://www.lunarvim.org/docs/configuration/language-features/linting-and-formatting
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    name = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespace
+    -- options such as `--line-width 80` become either `{"--line-width", "80"}` or `{"--line-width=80"}`
+    -- args = { "--print-width", "100" },
+    ---@usage only start in these filetypes, by default it will attach to all filetypes it supports
+    -- filetypes = { "typescript", "typescriptreact" },
+  },
+}
+
 lvim.plugins = {
   {
     "karb94/neoscroll.nvim",
