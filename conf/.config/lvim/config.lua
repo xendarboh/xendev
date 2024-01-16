8ddc92dd573b49d08832b3dc1d79bc6bf9fc5859
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2024-01-15 18:07:31 -0800

    fix(lunarvim): correct spellfile path

--- conf/.config/lvim/config.lua
+++ conf/.config/lvim/config.lua
@@ -7,7 +7,7 @@ vim.opt.relativenumber = true -- relative line numbers
 vim.opt.undofile = false      -- persistent undo
 vim.opt.whichwrap = "b,s"     -- allow left/right cursor keys to move prev/next
 
-vim.opt.spellfile = "~/.local/share/nvim/spell/en.utf-8.add"
+vim.opt.spellfile = os.getenv("HOME") .. "/.local/share/nvim/spell/en.utf-8.add"
 
 -- https://www.lunarvim.org/docs/configuration/appearance/statusline
 -- https://github.com/nvim-lualine/lualine.nvim/blob/master/README.md
