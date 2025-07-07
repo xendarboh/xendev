a30b692a78299d89112b34791682c5e8ff1ec6c1
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2025-07-07 11:00:21 -0700

    feat(lazyvim): set colorscheme=tokyonight-night

--- conf/.config/nvim-lazyvim/lua/plugins/colorscheme.lua
+++ conf/.config/nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,12 @@
 return {
   -- Add gruvbox colorscheme
   { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load a default colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
 }
