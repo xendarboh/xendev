02310768f63c7ec5b1e47373db2349628135dcde
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2026-03-11 14:46:26 -0700

    feat(lazyvim): move avante keymaps under <leader>av

--- conf/.config/nvim-lazyvim/lua/plugins/avante.lua
+++ conf/.config/nvim-lazyvim/lua/plugins/avante.lua
@@ -4,11 +4,34 @@ return {
     provider = "copilot",
   },
   keys = {
-    -- Enable menu grouping for "ai" commands in both Normal and Visual modes.
+    -- Disable default LazyVim avante keymaps under <leader>a
+    { "<leader>aa", false },
+    { "<leader>ac", false },
+    { "<leader>ae", false },
+    { "<leader>af", false },
+    { "<leader>ah", false },
+    { "<leader>am", false },
+    { "<leader>an", false },
+    { "<leader>ap", false },
+    { "<leader>ar", false },
+    { "<leader>as", false },
+    { "<leader>at", false },
+
+    -- Enable menu groupings in both Normal and Visual modes.
     { "<leader>a", "", desc = "+ai", mode = { "n", "x" } },
+    { "<leader>av", "", desc = "+avante", mode = { "n", "x" } },
 
     -- Ensure relevant commands are also in Visual mode.
-    { "<leader>aa", "<cmd>AvanteAsk<CR>", desc = "Ask Avante", mode = { "n", "v" } },
-    { "<leader>ae", "<cmd>AvanteEdit<CR>", desc = "Edit Avante", mode = { "n", "v" } },
+    { "<leader>ava", "<cmd>AvanteAsk<CR>", desc = "Ask Avante", mode = { "n", "v" } },
+    { "<leader>avc", "<cmd>AvanteChat<CR>", desc = "Chat with Avante" },
+    { "<leader>ave", "<cmd>AvanteEdit<CR>", desc = "Edit Avante", mode = { "n", "v" } },
+    { "<leader>avf", "<cmd>AvanteFocus<CR>", desc = "Focus Avante" },
+    { "<leader>avh", "<cmd>AvanteHistory<CR>", desc = "Avante History" },
+    { "<leader>avm", "<cmd>AvanteModels<CR>", desc = "Select Avante Model" },
+    { "<leader>avn", "<cmd>AvanteChatNew<CR>", desc = "New Avante Chat" },
+    { "<leader>avp", "<cmd>AvanteSwitchProvider<CR>", desc = "Switch Avante Provider" },
+    { "<leader>avr", "<cmd>AvanteRefresh<CR>", desc = "Refresh Avante" },
+    { "<leader>avs", "<cmd>AvanteStop<CR>", desc = "Stop Avante" },
+    { "<leader>avt", "<cmd>AvanteToggle<CR>", desc = "Toggle Avante" },
   },
 }
