9cf38a13a869c4726891fe3e041733a45977b6b2
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2026-03-16 22:43:52 -0700

    feat(fish): set default vi key bindings

--- conf/.config/fish/config.fish
+++ conf/.config/fish/config.fish
@@ -40,6 +40,8 @@ function fish_user_key_bindings
     fish_vi_key_bindings --no-erase insert
 end
 
+set -g fish_key_bindings fish_vi_key_bindings
+
 function fish_greeting
     # silence the greeting
 end
